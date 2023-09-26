6a797018775f62d6855bce3d45dd3c3c48240110
Olamiye Emmanuel <emmanuelolamiye@gmail.com>
2023-09-26 19:18:42 +0100

    making changes to files

--- main.py
+++ main.py
@@ -63,7 +63,56 @@ def room():
     if room is None or session.get('name') is None or room not in rooms:
         return redirect(url_for('home'))
     else:
-        return render_template('room.html')
+        return render_template('room.html', room=room, messages=rooms[room]["messages"])
+
+@socketio.on('message')
+def message(data):
+    room = session.get('room')
+    name = session.get('name')
+
+    if room not in rooms:
+        return
+
+    message_data = data.get('data')
+    content = {"name": name, "message": message_data}
+    send(content, to=room)
+    rooms[room]['messages'].append(content)
+    print(f"{session.get('name')} said: {data['data']}")
+
+    
+# When we initallize socket io, we are going to connect to the socket associated to the server that the website is on
+# As soon as we connect, there is an event that is admitted to our backend server called connect, the first thing we want to do is listen for that connection event and when the connection event occurs, we want to put the user into the specific room they are going to be in
+
+@socketio.on("connect") #The reason we are using socketio is because thats what we used for the initialization object for the socketIo library
+def connect(auth):
+    room = session.get('room')
+    name = session.get('name')
+
+    if not room and not name:
+        return
+    if room not in rooms:
+        leave_room(room)
+        return
+    
+    join_room(room)
+    send({"name": name, "message": "has entered the room"}, to=room)
+    rooms[room]["members"] += 1
+    print(f"{name} joined room {room}")
+
+@socketio.on("disconnect")
+def disconnect():
+    room = session.get("room")
+    name = session.get("name")
+    leave_room(room)
+
+    if room in rooms:
+        rooms[room]["members"] -= 1
+        if rooms[room]["members"] <= 0:
+            del rooms[room]
+
+    send({"name": name, "message": "has left the room"}, to=room)
+    print(f"{name} has left room {room}")
+
 
 if __name__ ==  '__main__':
     socketio.run(app, debug=True)
